a0b21a706e9be49b8370cd0438bbf8272eafc503
barashhh <kdstepanovv@gmail.com>
2024-10-05 02:38:00 +0300

    I did the second task, boom

---
+++ script.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+to_bin() {
+    IFS='.' read -r -a octets <<< "$1"
+    bin=""
+    for octet in "${octets[@]}"; do
+        bin+="$(printf "%08d" $(echo "obase=2;$octet" | bc))."
+    done
+    echo "${bin%?}"
+}
+
+if [ $# -eq 1 ]
+then
+    ipv4_ad=$1
+    bin_ad=$(to_bin "$ipv4_ad")
+    echo "$bin_ad"
+else
+    echo "Usage: $0 <IPv4 address>"
+    exit 1
+fi
